658ff06d078557c0976735197148866a2877837d
JakovStakovic <jakov.stakovic@gmail.com>
2024-11-06 09:32:08 +0100

    Cherry-pick
    Povratak na stari commit prije changea

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 print ("Pozdrav iz aplikacije od Jakova Stakovica! Moj broj indeksa je ##")
+=======
+print ("Pozdrav iz aplikacije od Jakova Stakovica!")
+>>>>>>> 453192e (Pozdrav aplikacija)
