dacb42b7befab9ca357e8a88d3af6f91a11826d6
Jakov Staković <116450819+JakovStakovic@users.noreply.github.com>
2024-11-06 10:00:53 +0100

    Cherry-pick
    Conflicts resolved

--- test.py
+++ test.py
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-print ("Pozdrav iz aplikacije od Jakova Stakovica! Moj broj indeksa je ##")
-=======
 print ("Pozdrav iz aplikacije od Jakova Stakovica!")
->>>>>>> 453192e (Pozdrav aplikacija)
+
